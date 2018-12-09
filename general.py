c745909efa719caa35e0497742d560ca32dff0cc
Suleiman Ahamed Mamman <suleimamman@gmail.com>
2018-12-09 05:56:25 +0100

    Adding and deleting links

--- general.py
+++ general.py
@@ -11,18 +11,27 @@ def crateWebsiteDir(directory):
 
 #Create queue and craweled file in not created
 def createDataFiles(projectName, baseURL):
-	queue = projectName + 'queue.txt';
-	craweled = projectName + 'craweled.txt';
+	queue = projectName + '/queue.txt';
+	craweled = projectName + '/craweled.txt';
 	if not os.path.isfile(queue):
 		createFile(queue, baseURL)
 	if not os.path.isfile(craweled):
-		createFile(queue, baseURL)
+		createFile(craweled, '')
 
 
 #Create a new file
-def createFile(path, data)
-	f = open(path, 'W')
+def createFile(path, data):
+	f = open(path, 'w')
 	f.write(data)
 	f.close()
 
+#Add data into an existing file
+def appendToFile(path, data):
+	with open(path, 'a') as file:
+		file.write(data + '\n')
 
+
+#Delete the content of a file
+def deleteFileContents(path):
+	with open(path, 'w')
+	pass
