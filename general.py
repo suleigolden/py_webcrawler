0c011aec5ba1524263c49cf10a2e9e708d39521b
Suleiman Ahamed Mamman <suleimamman@gmail.com>
2018-12-09 05:05:32 +0100

    Queue and Crawled Files

--- general.py
+++ general.py
@@ -1,10 +1,28 @@
 import os
 
 
-#Save Each crawed website in a saperate foulder
+#Save Each crawled website in a saperate foulder
 def crateWebsiteDir(directory):
 	if not os.path.exists(directory):
 		print('Creating Project...'+directory)
 		os.makedirs(directory)
+
+
+
+#Create queue and craweled file in not created
+def createDataFiles(projectName, baseURL):
+	queue = projectName + 'queue.txt';
+	craweled = projectName + 'craweled.txt';
+	if not os.path.isfile(queue):
+		createFile(queue, baseURL)
+	if not os.path.isfile(craweled):
+		createFile(queue, baseURL)
+
+
+#Create a new file
+def createFile(path, data)
+	f = open(path, 'W')
+	f.write(data)
+	f.close()
 
 
